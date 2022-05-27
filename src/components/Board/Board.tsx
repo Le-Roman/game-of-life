df1e62b0cac165ecea4a4ecdc3334c25531e6c68
Roman Lenshov <roman.lenshov@russianpost.ru>
2022-05-27 12:05:05 +0300

    Update Board.tsx

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,32 +1,41 @@
-import React, { FC } from "react";
-import { BoardSize, CellsData } from "../../types";
+import React, { FC, useState } from "react";
+import { BoardSize, CellsData, Coordinates } from "../../types";
 import Cell from "../Cell/Cell";
 import styled from "styled-components";
+import { generateBoardXY } from "../../utils";
+import { cellSize } from "../../constants";
 
 interface BoardStyledProps {
   boardSize: BoardSize;
+  cellSize: number;
 }
 
 const BoardStyled = styled.div`
-  border-right: 1px solid grey;
-  border-bottom: 1px solid grey;
-  width: ${(props: BoardStyledProps) => props.boardSize.x * 16}px;
-  height: ${(props: BoardStyledProps) => props.boardSize.y * 16}px;
+  width: ${({ boardSize: { x }, cellSize }: BoardStyledProps) =>
+    x * cellSize}px;
+  height: ${({ boardSize: { y }, cellSize }: BoardStyledProps) =>
+    y * cellSize}px;
 `;
 
 interface BoardProps {
   cellsData: CellsData;
   boardSize: BoardSize;
-  onCellClick: (cellData: number) => void;
+  onCellClick: (coord: Coordinates) => void;
 }
 
 const Board: FC<BoardProps> = ({ cellsData, boardSize, onCellClick }) => {
   return (
-    <BoardStyled boardSize={boardSize}>
+    <BoardStyled boardSize={boardSize} cellSize={cellSize}>
       {cellsData.map((row, iRow) => (
         <div key={iRow} style={{ display: "flex" }}>
           {row.map((cell, iCell) => (
-            <Cell key={iCell} cellData={cell} onClick={onCellClick} />
+            <Cell
+              key={`${iRow}${iCell}`}
+              cellData={cell}
+              x={iCell}
+              y={iRow}
+              onClick={onCellClick}
+            />
           ))}
         </div>
       ))}

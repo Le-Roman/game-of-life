05a843753b48b4159ccde23a6167ec859ac61a0a
Roman Lenshov <roman.lenshov@russianpost.ru>
2022-06-04 19:34:34 +0300

    update FormLogin.stories.tsx

--- src/components/FormLogin/FormLogin.stories.tsx
+++ src/components/FormLogin/FormLogin.stories.tsx
@@ -1,43 +1,34 @@
 // YourComponent.stories.ts|tsx
 
 import React from "react";
-
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-
 import FormLogin from "./FormLogin";
+import {
+  ContextState,
+  UserLoginContext,
+} from "../UserLoginProvider/UserLoginProvider";
+import { MemoryRouter } from "react-router-dom";
+
+const contextValue: ContextState = {
+  state: { login: "" },
+  onLogin: () => null,
+  onLogout: () => null,
+};
 
 export default {
   title: "Game_of_life/FormLogin",
   component: FormLogin,
   decorators: [
     (Story) => (
-      <div
-        style={{
-          width: 250,
-        }}
-      >
-        <Story />
-      </div>
+      <UserLoginContext.Provider value={{ ...contextValue }}>
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      </UserLoginContext.Provider>
     ),
   ],
 } as ComponentMeta<typeof FormLogin>;
 
-const Template: ComponentStory<typeof FormLogin> = (args) => (
-  <FormLogin {...args} />
-);
+const Template: ComponentStory<typeof FormLogin> = () => <FormLogin />;
 
-export const FormLoginIsLogout = Template.bind({});
-
-FormLoginIsLogout.args = {
-  login: "",
-  onLogin: () => null,
-  onLogout: () => null,
-};
-
-export const FormLoginIsLogin = Template.bind({});
-
-FormLoginIsLogin.args = {
-  login: "UserName",
-  onLogin: () => null,
-  onLogout: () => null,
-};
+export const FormLoginDefault = Template;

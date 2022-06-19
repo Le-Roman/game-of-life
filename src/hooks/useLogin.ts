43d66089e44acaf747b63d85bae0d067664af4c8
Roman Lenshov <roman.lenshov@russianpost.ru>
2022-06-19 22:18:35 +0300

    Create hooks

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ROUTE } from "../constants";
+import { selectIsLogined } from "../state/userSlice/userSelectors";
+import { useTypedSelector } from "./useTypedSelector";
+
+export const useLogin = () => {
+  const navigate = useNavigate();
+  const isLogined = useTypedSelector(selectIsLogined);
+
+  useEffect(() => {
+    isLogined ? navigate(ROUTE.ROOT) : navigate(ROUTE.AUTH);
+  }, [isLogined]);
+
+  return { isLogined };
+};
